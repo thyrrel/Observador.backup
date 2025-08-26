b574c47fce00af21d21fc66ebb889b0625b38ef3
thyrrel <thyrrel@gmail.com>
2025-08-26 15:36:15 -0300

    Initialize Flutter project structure: lib/providers/app_provider.dart

---
+++ lib/providers/app_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class AppProvider extends ChangeNotifier {
+  int _counter = 0;
+  
+  int get counter => _counter;
+  
+  void incrementCounter() {
+    _counter++;
+    notifyListeners();
+  }
+  
+  void resetCounter() {
+    _counter = 0;
+    notifyListeners();
+  }
+}
